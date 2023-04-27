c3c2683a13cde94d4d99f523cf4170384b00c34c
Alexander Seiler <seileralex@gmail.com>
2023-04-27 11:13:12 +0200

    Correct some typos (#62)
    Signed-off-by: Alexander Seiler <seileralex@gmail.com>

--- dinov2/loss/ibot_patch_loss.py
+++ dinov2/loss/ibot_patch_loss.py
@@ -87,7 +87,7 @@ class iBOTPatchLoss(nn.Module):
             Q /= torch.sum(Q, dim=0, keepdim=True)
             Q /= B
 
-        Q *= B  # the colomns must sum to 1 so that Q is an assignment
+        Q *= B  # the columns must sum to 1 so that Q is an assignment
         return Q.t()
 
     def forward(self, student_patch_tokens, teacher_patch_tokens, student_masks_flat):
