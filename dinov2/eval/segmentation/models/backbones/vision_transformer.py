a9ccb79ec88e070eff25b99ac727b1535b656652
Patrick Labatut <plabatut@meta.com>
2023-08-30 05:46:33 -0700

    Lint + copyright headers

--- dinov2/eval/segmentation/models/backbones/vision_transformer.py
+++ dinov2/eval/segmentation/models/backbones/vision_transformer.py
@@ -1,11 +1,8 @@
-<<<<<<< HEAD
 # Copyright (c) Meta Platforms, Inc. and affiliates.
 #
 # This source code is licensed under the Apache License, Version 2.0
 # found in the LICENSE file in the root directory of this source tree.
 
-=======
->>>>>>> d07608f (Add segmentation code)
 from mmcv.runner import BaseModule
 from mmseg.models.builder import BACKBONES
 
