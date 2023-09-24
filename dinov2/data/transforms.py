bfa7bada046cd4ccaa04afe6f7ce50ab8e0c2647
MohammedSB <75508181+MohammedSB@users.noreply.github.com>
2023-09-24 13:18:41 +0300

    fixed rescaling across multiple channels
    Added correct rescaling across multiple channels and added CHW permutation for NumPy arrays.

--- dinov2/data/transforms.py
+++ dinov2/data/transforms.py
@@ -32,8 +32,8 @@ class RescaleImage:
             raise TypeError("Input should be of type numpy.ndarray or torch.Tensor")
 
         # Rescale the tensor to [0, 1]
-        min_val = image.min()
-        max_val = image.max()
+        min_val = image.reshape(image.shape[0], -1).min(dim=1)[0].reshape(-1, 1, 1)
+        max_val = image.reshape(image.shape[0], -1).max(dim=1)[0].reshape(-1, 1, 1)
         return (image - min_val) / (max_val - min_val)
 
 
@@ -52,7 +52,8 @@ class MaybeToTensor(transforms.PILToTensor):
         if isinstance(pic, torch.Tensor):
             return pic
         if isinstance(pic, np.ndarray):
-            return torch.from_numpy(pic)
+            pic = torch.from_numpy(pic)
+            return pic.permute(2, 0, 1) 
         return super().__call__(pic)
 
 # Use timm's names
