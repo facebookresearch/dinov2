b507fbcf50bfe225a1bd7bb70b2323bb0c5033b5
Patrick Labatut <60359573+patricklabatut@users.noreply.github.com>
2023-09-30 20:01:59 +0200

    Minor config tweaks (#246)
    Ignore import warnings in hubconf and trigger lint workflow on PR.

--- dinov2/hub/utils.py
+++ dinov2/hub/utils.py
@@ -3,8 +3,6 @@
 # This source code is licensed under the Apache License, Version 2.0
 # found in the LICENSE file in the root directory of this source tree.
 
-import torch.nn as nn
-
 _DINOV2_BASE_URL = "https://dl.fbaipublicfiles.com/dinov2"
 
 
