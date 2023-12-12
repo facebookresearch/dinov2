856a671ec4c133156216bf81e277918522d5ab5f
Lenz <lorenz.rumberger@gmail.com>
2023-12-12 17:04:29 +0100

    Update train.sh

--- train.sh
+++ train.sh
@@ -1,5 +1,2 @@
 #!/bin/bash
-torchrun --standalone --nnodes=1 --nproc_per_node=8 dinov2/run/train/train.py \
-    --config-file dinov2/configs/train/vitl16_short.yaml \
-    --output-dir test_experiment \
-    train.dataset_path=ImageNet:split=TRAIN:root=/fast/AG_Kainmueller/plankton/data/ImageNetSubset/imagenette2:extra=/fast/AG_Kainmueller/plankton/data/ImageNetSubset/imagenette2/EXTRA
+torchrun --standalone --nnodes=1 --nproc_per_node=8 dinov2/run/train/train.py --config-file dinov2/configs/train/whoi.yaml --output-dir /fast/AG_Kainmueller/plankton/test_experiment
