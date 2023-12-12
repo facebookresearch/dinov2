974e02fb517aed6a38e3a1bc2b376d23d14afceb
Lenz <lorenz.rumberger@gmail.com>
2023-12-12 17:05:06 +0100

    Update train.sh

--- train.sh
+++ train.sh
@@ -1,2 +1,1 @@
-#!/bin/bash
 torchrun --standalone --nnodes=1 --nproc_per_node=8 dinov2/run/train/train.py --config-file dinov2/configs/train/whoi.yaml --output-dir /fast/AG_Kainmueller/plankton/test_experiment
