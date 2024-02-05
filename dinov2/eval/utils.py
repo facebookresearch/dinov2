560a093547caff2ddc36009cb1c62285076786e3
cm090999 <colin.merk@gmx.ch>
2024-02-05 11:40:26 +0100

    set device cuda

--- dinov2/eval/utils.py
+++ dinov2/eval/utils.py
@@ -111,7 +111,7 @@ def extract_features(model, dataset, batch_size, num_workers, gather_on_cpu=Fals
 
 @torch.inference_mode()
 def extract_features_with_dataloader(model, data_loader, sample_count, gather_on_cpu=False):
-    gather_device = torch.device("cpu") if gather_on_cpu else torch.device("cuda")
+    gather_device = torch.device("cpu") if gather_on_cpu else torch.cuda()
     metric_logger = MetricLogger(delimiter="  ")
     features, all_labels = None, None
     for samples, (index, labels_rank) in metric_logger.log_every(data_loader, 10):
