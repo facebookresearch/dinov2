c91f919a01849e0b3b81b37f81cdce3e4685a9ce
cm090999 <colin.merk@gmx.ch>
2024-02-26 14:24:18 +0100

    add import

--- dinov2/data/loaders.py
+++ dinov2/data/loaders.py
@@ -10,7 +10,7 @@ from typing import Any, Callable, List, Optional, TypeVar
 import torch
 from torch.utils.data import Sampler
 
-from .datasets import ImageNet, ImageNet22k, EyePACSDataset
+from .datasets import ImageNet, ImageNet22k, EyePACSDataset, BaseFolderDataset
 from .samplers import EpochSampler, InfiniteSampler, ShardedInfiniteSampler
 
 
@@ -60,6 +60,8 @@ def _parse_dataset_str(dataset_str: str):
         class_ = ImageNet22k
     elif name == "eyepacs":
         class_ = EyePACSDataset
+    elif name == "ImageFolder":
+        class_ = BaseFolderDataset
     else:
         raise ValueError(f'Unsupported dataset "{name}"')
 
