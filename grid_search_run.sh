33b487dbd28a3bab22058185378fccaa76229f8b
cm090999 <colin.merk@gmx.ch>
2024-03-04 14:45:14 +0100

    launch file on colins machine

--- grid_search_run.sh
+++ grid_search_run.sh
@@ -1,8 +1,9 @@
 # config_files=(ssl_default_config_small_noKoleo ssl_default_config_base_noKoleo ssl_default_config_base_withKoleo ssl_default_config_small_withKoleo)
 
-config_files=(ssl_default_config_base_noKoleo)
+config_files=(base_3_blocks)
+output_dir="/cluster/scratch/cmerk/dinov2_accum_12_low_lr_2_filtered_dataset_3_block_no_patches"
 
 for config in "${config_files[@]}"
 do
-    sbatch grid_search.slurm $config
+    sbatch grid_search.slurm $config $output_dir
 done
