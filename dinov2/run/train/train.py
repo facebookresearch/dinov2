479acef8f1b9e72c386bef383c48b782f65d3e33
JW <252653889@qq.com>
2024-05-24 18:05:27 +0800

    add the sys.path way to help finding the dinov2 in using VSCODE with linux_x64

--- dinov2/run/train/train.py
+++ dinov2/run/train/train.py
@@ -7,6 +7,8 @@ import logging
 import os
 import sys
 
+sys.path.append(os.path.dirname(os.path.abspath(__file__)) + '/../../..')
+
 from dinov2.logging import setup_logging
 from dinov2.train import get_args_parser as get_train_args_parser
 from dinov2.run.submit import get_args_parser, submit_jobs
