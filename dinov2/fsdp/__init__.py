5d31bf88fc0a79ebe30deb06e0b014051aea6f9f
Adrian Wolny <adrian.wolny@bayer.com>
2024-08-20 18:06:13 +0200

    make FSDP compatible with PyTorch 2.4

--- dinov2/fsdp/__init__.py
+++ dinov2/fsdp/__init__.py
@@ -63,10 +63,9 @@ def is_sharded_fsdp(x):
 
 
 def free_if_fsdp(x):
-    if is_sharded_fsdp(x):
-        handles = x._handles
-        true_list = [True for h in handles]
-        _reshard(x, handles, true_list)
+    if is_sharded_fsdp(x) and x._has_params:
+        handle = x._handle
+        _reshard(x, handle, True)
 
 
 def get_fsdp_modules(x):
