fix logging and add koleo and normalization

dinov2/loss/dino_clstoken_loss.py
@@ -65,37 +65,41 @@ class DINOLoss(nn.Module):
     def forward(
         self, student_output_list, teacher_out_softmaxed_centered_list, graph=None
     ):
-        print("❤️ [DEBUG] Graph provided:", graph is not None)
-
-        # print graph shape
         if graph is not None:
-            print("Graph shape:", graph.shape)
+            student_cls_tokens = torch.cat(
+                student_output_list, dim=0
+            )  # shape: (N_CROPS * BS, D)
+            teacher_softmaxed_cls = torch.cat(
+                list(teacher_out_softmaxed_centered_list), dim=0
+            )  # shape: (N_CROPS * BS, D)
 
-        student_cls_tokens = torch.cat(
-            student_output_list, dim=0
-        )  # shape: (N_CROPS * BS, D)
-        teacher_cls_tokens = torch.cat(
-            list(teacher_out_softmaxed_centered_list), dim=0
-        )  # shape: (N_CROPS * BS, D)
+            log_p_s = F.log_softmax(student_cls_tokens / self.student_temp, dim=-1)
+            teacher_softmaxed_cls = teacher_softmaxed_cls.to(log_p_s.dtype)
 
-        p_s = F.softmax(student_cls_tokens / self.student_temp, dim=-1)
-        log_p_t = F.log_softmax(teacher_cls_tokens / self.student_temp, dim=-1)
+            # shape: (N_CROPS * BS, N_CROPS * BS)
+            H = -(log_p_s @ teacher_softmaxed_cls.t())
 
-        log_p_t = log_p_t.to(p_s.dtype)
-        H = p_s @ log_p_t.T
-
-        if graph is not None:
-            graph = graph.to(H.device)
             assert graph.shape == H.shape, (
                 f"Graph shape {graph.shape} must match entropy matrix {H.shape}"
             )
-            L = graph * H
+
+            # Rem: Should remove the main diag of G?
+            # TODO eq (3) of https://arxiv.org/pdf/2104.14294
+
+            G = graph.to(H.device)
+            total_loss = (G * H).sum() / G.sum()
+
+            return total_loss
+
         else:
-            L = H
+            total_loss = 0
+            for s in student_output_list:
+                lsm = F.log_softmax(s / self.student_temp, dim=-1)
+                for t in teacher_out_softmaxed_centered_list:
+                    loss = torch.sum(t * lsm, dim=-1)
+                    total_loss -= loss.mean()
 
-        loss = -L.sum()  # TODO divide by number of elements if needed to avg
-
-        return loss
+            return total_loss
 
     @torch.no_grad()
     def update_center(self, teacher_output):

dinov2/loss/koleo_loss.py
@@ -24,7 +24,7 @@ class KoLeoLoss(nn.Module):
         super().__init__()
         self.pdist = nn.PairwiseDistance(2, eps=1e-8)
 
-    def pairwise_NNs_inner(self, x):
+    def pairwise_NNs_inner(self, x, graph=None):
         """
         Pairwise nearest neighbors for L2-normalized vectors.
         Uses Torch rather than Faiss to remain on GPU.
@@ -33,18 +33,27 @@ class KoLeoLoss(nn.Module):
         dots = torch.mm(x, x.t())
         n = x.shape[0]
         dots.view(-1)[:: (n + 1)].fill_(-1)  # Trick to fill diagonal with -1
+
+        # if graph is provided, find the neearest neighbors from different class
+        # rem: make sure graph and dots have same shape, i.e student_output has been gathered
+        if graph is not None:
+            assert dots.shape == graph.shape, (
+                f"Shape mismatch: {dots.shape} vs {graph.shape}"
+            )
+            dots[graph > 0] = -1
+
         # max inner prod -> min distance
         _, I = torch.max(dots, dim=1)  # noqa: E741
         return I
 
-    def forward(self, student_output, eps=1e-8):
+    def forward(self, student_output, eps=1e-8, graph=None):
         """
         Args:
             student_output (BxD): backbone output of student
         """
         with torch.cuda.amp.autocast(enabled=False):
             student_output = F.normalize(student_output, eps=eps, p=2, dim=-1)
-            I = self.pairwise_NNs_inner(student_output)  # noqa: E741
+            I = self.pairwise_NNs_inner(student_output, graph)  # noqa: E741
             distances = self.pdist(student_output, student_output[I])  # BxD, BxD -> B
             loss = -torch.log(distances + eps).mean()
         return loss

dinov2/train/ssl_meta_arch.py
@@ -165,8 +165,6 @@ class SSLMetaArch(nn.Module):
             loss.backward()
 
     def forward_backward(self, images, teacher_temp, graph=None):
-        print("❤️ [DEBUG] FORWARD BACKWARD Graph provided:", graph is not None)
-
         n_global_crops = 2
         assert n_global_crops == 2
         n_local_crops = self.cfg.crops.local_crops_number
@@ -376,21 +374,18 @@ class SSLMetaArch(nn.Module):
             )[:n_masked_patches]
 
         if n_local_crops > 0:
+            dino_local_crops_loss = self.dino_loss(
+                student_output_list=student_local_cls_tokens_after_head.chunk(
+                    n_local_crops
+                ),
+                teacher_out_softmaxed_centered_list=teacher_dino_softmaxed_centered_list,
+                graph=graph["semisup_graph"] if graph is not None else None,
+            )
+
             if graph is None:
-                dino_local_crops_loss = self.dino_loss(
-                    student_output_list=student_local_cls_tokens_after_head.chunk(
-                        n_local_crops
-                    ),
-                    teacher_out_softmaxed_centered_list=teacher_dino_softmaxed_centered_list,
-                ) / (n_global_crops_loss_terms + n_local_crops_loss_terms)
-            else:
-                dino_local_crops_loss = self.dino_loss(
-                    student_output_list=student_local_cls_tokens_after_head.chunk(
-                        n_local_crops
-                    ),
-                    teacher_out_softmaxed_centered_list=teacher_dino_softmaxed_centered_list,
-                    graph=graph["semisup_graph"],
-                ) / (n_global_crops_loss_terms + n_local_crops_loss_terms)
+                dino_local_crops_loss /= (
+                    n_global_crops_loss_terms + n_local_crops_loss_terms
+                )
 
             # store for display
             loss_dict["dino_local_crops_loss"] = dino_local_crops_loss
@@ -402,29 +397,17 @@ class SSLMetaArch(nn.Module):
         loss_scales = 2  # this is here since we process global crops together
 
         if do_dino:
-            # compute loss
+            dino_global_crops_loss = self.dino_loss(
+                student_output_list=[student_global_cls_tokens_after_head],
+                teacher_out_softmaxed_centered_list=[
+                    teacher_dino_softmaxed_centered_list.flatten(0, 1)
+                ],  # these were chunked and stacked in reverse so A is matched to B
+                graph=graph["semisup_graph_global"] if graph is not None else None,
+            )
+
             if graph is None:
-                dino_global_crops_loss = (
-                    self.dino_loss(
-                        student_output_list=[student_global_cls_tokens_after_head],
-                        teacher_out_softmaxed_centered_list=[
-                            teacher_dino_softmaxed_centered_list.flatten(0, 1)
-                        ],  # these were chunked and stacked in reverse so A is matched to B
-                    )
-                    * loss_scales
-                    / (n_global_crops_loss_terms + n_local_crops_loss_terms)
-                )
-            else:
-                dino_global_crops_loss = (
-                    self.dino_loss(
-                        student_output_list=[student_global_cls_tokens_after_head],
-                        teacher_out_softmaxed_centered_list=[
-                            teacher_dino_softmaxed_centered_list.flatten(0, 1)
-                        ],  # these were chunked and stacked in reverse so A is matched to B
-                        graph=graph["semisup_graph_global"],
-                    )
-                    * loss_scales
-                    / (n_global_crops_loss_terms + n_local_crops_loss_terms)
+                dino_global_crops_loss *= loss_scales / (
+                    n_global_crops_loss_terms + n_local_crops_loss_terms
                 )
 
             loss_dict["dino_global_crops_loss"] = dino_global_crops_loss
@@ -435,13 +418,25 @@ class SSLMetaArch(nn.Module):
             student_cls_tokens = student_global_cls_tokens
 
             if self.do_koleo:
-                koleo_loss = self.cfg.dino.koleo_loss_weight * sum(
-                    self.koleo_loss(p) for p in student_cls_tokens.chunk(2)
-                )  # we don't apply koleo loss between cls tokens of a same image
+                per_view_graph = graph
+                # keep one view graph for koleo
+                if per_view_graph is not None:
+                    graph_size = per_view_graph["semisup_graph_global"].shape[0] // 2
+                    per_view_graph = graph["semisup_graph_global"][
+                        :graph_size, :graph_size
+                    ]
+
+                # we don't apply koleo loss between cls tokens of a same image
+                # koleo loss per global crop
+                student_views_cls = student_cls_tokens.chunk(2)
+                koleo_weight = self.cfg.dino.koleo_loss_weight
+                koleo_loss = koleo_weight * sum(
+                    self.koleo_loss(p, graph=per_view_graph) for p in student_views_cls
+                )
+
+                # this is to display the same losses as before but we can remove eventually
                 loss_accumulator += koleo_loss
-                loss_dict["koleo_loss"] = (
-                    koleo_loss / loss_scales
-                )  # this is to display the same losses as before but we can remove eventually
+                loss_dict["koleo_loss"] = koleo_loss / loss_scales
 
         if do_ibot:
             # compute loss